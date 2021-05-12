dcfe0c74a340ebf4fac13eaac9788e8a705659aa
doronlavi <45287222+doronlavi@users.noreply.github.com>
2021-05-12 15:10:25 +0300

    updated DatabaseHandler and Parser
    They work together!

---
+++ DatabaseHandler.py
@@ -0,0 +1,150 @@
+import sqlite3
+from sqlite3 import Error
+
+
+class DatabaseHandler:
+    __SQL_CREATE_TESTS_DETAILS = """CREATE TABLE IF NOT EXISTS tests_details(  
+                                  run_id integer,
+                                  class_name text,
+                                  test_name text, 
+                                  did_pass integer CHECK(did_pass=1 or did_pass=0),
+                                  error text,
+                                  run_time text,
+                                  PRIMARY KEY(run_id, class_name, test_name)
+                                  FOREIGN KEY (run_id) REFERENCES run_summary (run_id)
+                                  );"""
+
+    __SQL_CREATE_RUN_SUMMARY = """CREATE TABLE IF NOT EXISTS run_summary(
+                                run_id integer PRIMARY KEY,
+                                errors integer,
+                                passed integer,
+                                failed integer,
+                                skipped integer,
+                                run_time text,
+                                timestemp text
+                                );"""
+
+    __SQL_CREATE_COVERAGE = """CREATE TABLE IF NOT EXISTS coverage(
+                                run_id integer,
+                                line_rate real,
+                                package_name text,
+                                file_name text,
+                                class_name text,
+                                PRIMARY KEY(run_id, package_name, file_name, class_name)
+                                FOREIGN KEY (run_id) REFERENCES run_summary (run_id)
+                                );"""
+
+    __SQL_CREATE_COVERAGE_SUMMARY = """CREATE TABLE IF NOT EXISTS coverage_summary(
+                                      run_id integer PRIMARY KEY,
+                                      line_rate real,
+                                      lines_valid integer,
+                                      timestemp text,
+                                      FOREIGN KEY (run_id) REFERENCES run_summary (run_id)
+                                    );"""
+
+    __SQL_CREATE_FUNCTIONS_DETAILS = """CREATE TABLE IF NOT EXISTS functions_details(
+                                      run_id integer,
+                                      file_name text,
+                                      function_name text,
+                                      is_tested integer CHECK(is_tested=1 or is_tested=0),
+                                      PRIMARY KEY(run_id, file_name, file_name, function_name)
+                                      FOREIGN KEY (run_id) REFERENCES run_summary (run_id)
+                                    );"""
+
+    __SQL_INSERT_TESTS_DETAILS = """INSERT INTO tests_details(run_id,class_name,test_name,did_pass,error,run_time)
+                                VALUES(?,?,?,?,?,?)"""
+
+    __SQL_INSERT_RUN_SUMMARY = """INSERT INTO run_summary(run_id,errors,passed,failed,skipped,run_time,timestemp)
+                                VALUES(?,?,?,?,?,?,?)"""
+
+    __SQL_INSERT_COVERAGE = """INSERT INTO coverage(run_id,line_rate,package_name,file_name,class_name)
+                                VALUES(?,?,?,?,?)"""
+
+    __SQL_INSERT_COVERAGE_SUMMARY = """INSERT INTO coverage_summary(run_id,line_rate,lines_valid,timestemp)
+                                VALUES(?,?,?,?)"""
+
+    __SQL_INSERT_FUNCTIONS_DETAILS = """INSERT INTO functions_details(run_id,file_name,function_name,is_tested)
+                                VALUES(?,?,?,?)"""
+
+    def __add_table(self, create_table_sql):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(create_table_sql)
+        except Error as e:
+            print(e)
+
+    def __create_tables(self):
+        self.__add_table(DatabaseHandler.__SQL_CREATE_TESTS_DETAILS)
+        self.__add_table(DatabaseHandler.__SQL_CREATE_RUN_SUMMARY)
+        self.__add_table(DatabaseHandler.__SQL_CREATE_COVERAGE)
+        self.__add_table(DatabaseHandler.__SQL_CREATE_COVERAGE_SUMMARY)
+        self.__add_table(DatabaseHandler.__SQL_CREATE_FUNCTIONS_DETAILS)
+
+    def __open_connection(self):
+        try:
+            self.connection = sqlite3.connect(self.projectPath)
+            return True
+
+        except Error as e:
+            self.connection = None
+            print("Can't create Codearge database: " + str(e))
+            return False
+
+    def __init__(self, project_path):
+        self.projectPath = project_path + "/coderage.db"
+        self.connection = None
+
+        if not self.__open_connection():
+            exit(1)
+
+        self.__create_tables()
+        self.connection.close()
+
+    def __insert_to_table(self, sql_command, data):
+        """
+        Create a new project into the projects table
+        :param conn:
+        :param project:
+        :return: project id
+        """
+        self.__open_connection()
+        for line in data:
+            cur = self.connection.cursor()
+            cur.execute(sql_command, line)
+            self.connection.commit()
+
+    def insert_tests_details(self, data):
+        self.__insert_to_table(DatabaseHandler.__SQL_INSERT_TESTS_DETAILS, data)
+
+    def insert_run_summary(self, data):
+        self.__insert_to_table(DatabaseHandler.__SQL_INSERT_RUN_SUMMARY, data)
+
+    def insert_coverage(self, data):
+        self.__insert_to_table(DatabaseHandler.__SQL_INSERT_COVERAGE, data)
+
+    def insert_coverage_summary(self, data):
+        self.__insert_to_table(DatabaseHandler.__SQL_INSERT_COVERAGE_SUMMARY, data)
+
+    def insert_functions_details(self, data):
+        self.__insert_to_table(DatabaseHandler.__SQL_INSERT_FUNCTIONS_DETAILS, data)
+
+    def select_all(self):
+        self.__open_connection()
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM functions_details")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    def get_last_run_id(self):
+        self.__open_connection()
+        cur = self.connection.cursor()
+        cur.execute("SELECT max(run_id) FROM run_summary")
+        rows = cur.fetchall()
+        return rows[0][0]
+
+
+
+# (r"C:\Users\Doron\Desktop\OpenSource\parsing\coderage.db")
--- Parser.py
+++ Parser.py
@@ -1,97 +1,116 @@
 import os
 import xml.etree.ElementTree as et
 
-def parsePytest(file_path):
-    tree = et.parse(file_path)
-    root = tree.getroot()
 
-    passTests = {}
-    failTests = {}
-    test_rows = []
-    summary_rows = []
-    runId = 0
+class Parser:
 
-    # TODO change timestamp to same string format in all tables
-    for element in root:
-        summary_rows.append((runId, int(element.attrib["errors"]), int(element.attrib["tests"]) - int(element.attrib["failures"]), int(element.attrib["failures"]), int(element.attrib["skipped"]), float(element.attrib["time"]), element.attrib["timestamp"]))
-        for child in element:
-            if not len(child):
-                passTests[child.attrib["name"]] = child.attrib["classname"]
-                test_rows.append([runId, child.attrib["classname"], child.attrib["name"], 1, None, float(child.attrib["time"])])
-            else:
-                for grandchild in child:
-                    failTests[child.attrib["name"]] = grandchild.text
-                    test_rows.append(
-                        (runId, child.attrib["classname"], child.attrib["name"], 0, grandchild.text, float(child.attrib["time"])))
+    def __init__(self, db, directory):
+        self.db = db
+        self.directory = directory
+        self.passTests = {}
+        self.failTests = {}
+        self.cover = {}
+        self.uncover = {}
+        self.runId = self.db.get_last_run_id()
 
-    # call insert function
+        self.parse_pytest()
+        self.parseCoverage()
+        self.parseAnnotate()
 
-    print("----------- Passed / Failed -----------")
-    print(passTests)
-    print(failTests)
-    print(test_rows)
-    print(summary_rows)
+    def parse_pytest(self):
+        file_path = self.directory + "/filetest.xml"
+        tree = et.parse(file_path)
+        root = tree.getroot()
+        test_rows = []
+        summary_rows = []
 
-def parseCoverage(file_path):
-    tree = et.parse(file_path)
-    root = tree.getroot()
-    runId = 0
-    coverarge_rows = []
-    sum_rows = []
+        # TODO change timestamp to same string format in all tables
+        for element in root:
+            summary_rows.append((self.runId, int(element.attrib["errors"]),
+                                 int(element.attrib["tests"]) - int(element.attrib["failures"]),
+                                 int(element.attrib["failures"]),
+                                 int(element.attrib["skipped"]),
+                                 float(element.attrib["time"]),
+                                 element.attrib["timestamp"]))
+            for child in element:
+                if not len(child):
+                    self.passTests[child.attrib["name"]] = child.attrib["classname"]
+                    test_rows.append((self.runId, child.attrib["classname"], child.attrib["name"], 1, None, float(child.attrib["time"])))
+                else:
+                    for grandchild in child:
+                        self.failTests[child.attrib["name"]] = grandchild.text
+                        test_rows.append((self.runId, child.attrib["classname"], child.attrib["name"], 0, grandchild.text, float(child.attrib["time"])))
 
-    cover = {}
-    uncover = {}
-    sum_rows.append((runId, float(root.attrib["line-rate"]), int(root.attrib["lines-valid"]), root.attrib["timestamp"]))
-    for element in root:
-        if element.tag == 'packages':
-            for package in element:
-                for classes in package:
-                    for testClass in classes:
-                        coverarge_rows.append((runId, float(testClass.attrib["line-rate"]), package.attrib["name"], testClass.attrib["filename"], testClass.attrib["name"]))
-                        for classContest in testClass:
-                            if classContest.tag == 'lines':
-                                for line in classContest:
-                                    className = testClass.attrib['name']
-                                    lineNumber = line.attrib['number']
-                                    if line.attrib['hits'] == '1':
-                                        try:
-                                            cover[className].add(lineNumber)
-                                        except KeyError:
-                                            cover[className] = {lineNumber}
-                                    else:
-                                        try:
-                                            uncover[className].add(lineNumber)
-                                        except KeyError:
-                                            uncover[className] = {lineNumber}
+        self.db.insert_tests_details(test_rows)
+        self.db.insert_run_summary(summary_rows)
 
-    #insert rows
-    print("----------- Coverage -----------")
-    print(cover)
-    print(uncover)
-    print(sum_rows)
-    print(coverarge_rows)
+        print("----------- Passed / Failed -----------")
+        print(test_rows)
+        print(summary_rows)
 
-def parseAnnotate(directory):
-    if os.path.isdir(directory):
-        for filename in os.listdir(directory):
-            if filename.endswith(",cover"):
-                parseAnnotateFile(directory + "\\" + filename) #TODO fix \\
-    if os.path.isfile(directory):
-        parseAnnotateFile(directory)
+    def parseCoverage(self):
+        file_path = self.directory + "/coverage.xml"
+        tree = et.parse(file_path)
+        root = tree.getroot()
+        coverage_rows = []
+        sum_rows = []
 
-def parseAnnotateFile(file_path):
-    function_details_rows = []
-    runId = 0
-    file_name = file_path.split('\\')[-1]
-    file_name = file_name[:file_name.index(',')]
-    with open(file_path, 'r') as file:
-        functions = file.read().split('> def')
-        for function in functions[1:]:
-            function_name = function.split('\n')[0]
-            function_name = function_name[:function_name.index("(")]
-            is_tested = 0
-            if '>' in function:
-                is_tested = 1
-            function_details_rows.append([runId, file_name, function_name, is_tested])
-    print(function_details_rows)
+        sum_rows.append((self.runId, float(root.attrib["line-rate"]),
+                         int(root.attrib["lines-valid"]), root.attrib["timestamp"]))
+        for element in root:
+            if element.tag == 'packages':
+                for package in element:
+                    for classes in package:
+                        for testClass in classes:
+                            coverage_rows.append((self.runId,
+                                                   float(testClass.attrib["line-rate"]), package.attrib["name"], testClass.attrib["filename"], testClass.attrib["name"]))
+                            for classContest in testClass:
+                                if classContest.tag == 'lines':
+                                    for line in classContest:
+                                        className = testClass.attrib['name']
+                                        lineNumber = line.attrib['number']
+                                        if line.attrib['hits'] == '1':
+                                            try:
+                                                self.cover[className].add(lineNumber)
+                                            except KeyError:
+                                                self.cover[className] = {lineNumber}
+                                        else:
+                                            try:
+                                                self.uncover[className].add(
+                                                    lineNumber)
+                                            except KeyError:
+                                                self.uncover[className] = {
+                                                    lineNumber}
 
+        self.db.insert_coverage(coverage_rows)
+        self.db.insert_coverage_summary(sum_rows)
+
+        print("----------- Coverage -----------")
+        print(sum_rows)
+        print(coverage_rows)
+
+    def parseAnnotate(self):
+        if os.path.isdir(self.directory):
+            for filename in os.listdir(self.directory):
+                if filename.endswith(",cover"):
+                    self.parseAnnotateFile(self.directory + "/" + filename)
+        if os.path.isfile(self.directory):
+            self.parseAnnotateFile(self.directory)
+
+    def parseAnnotateFile(self, file_path):
+        function_details_rows = []
+        runId = 0
+        file_name = file_path.split('\\')[-1]
+        file_name = file_name[:file_name.index(',')]
+        with open(file_path, 'r') as file:
+            functions = file.read().split('> def')
+            for function in functions[1:]:
+                function_name = function.split('\n')[0]
+                function_name = function_name[:function_name.index("(")]
+                is_tested = 0
+                if '>' in function:
+                    is_tested = 1
+                function_details_rows.append((runId, file_name, function_name, is_tested))
+
+        self.db.insert_functions_details(function_details_rows)
+        print(function_details_rows)
