93ffda2413be5fdfb9380f57796572f11b4d324d
shaked.kialy <shaked.kialy@huji.mail.ac.il>
2021-05-17 22:17:05 +0300

    added pytest extra args support and argument validation

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ The purpose of this project is to enable easy and efficient analyzing and conclu
 requirements for running: 
     1. add __init__.py file to the code package you want to test.
     2. name your test files as *test.py
+    3. command line for example:
+    python main.py module=module1,module2 tests=test1,test2, out_dir=results
--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import sys
 from Parser import *
 from DatabaseHandler import *
 
+def parse_args(argv):
+    code_path, test_path, output_path, extra_args = "", "", "", ""
+    for arg in argv:
+        if "module" in arg:
+            modules = arg.split("=")[1]
+            code_path = modules.split(",")
+        if "tests" in arg:
+            test_path = arg.split("=")[1]
+        if "out_dir" in arg:
+            output_path = arg.split("=")[1]
+        if len(sys.argv) > 4:
+            extra_args = sys.argv[4:len(sys.argv)]
+        extra_args = " ".join(extra_args)
+    if code_path == "":
+        raise Exception("Missing modules to cover")
+    if test_path == "":
+        test_path = "Tests"
+    if output_path == "":
+        output_path = "Results"
+    return code_path, test_path, output_path, extra_args
+
 
 if __name__ == "__main__":
-    #TODO: more elegant way to parse modules. add names for parameters
-    num_modules = int(sys.argv[1])
-    code_path = []
-    for i in range(num_modules):
-        code_path.append(sys.argv[2 + i])
-    test_path = sys.argv[2 + num_modules]
-    output_path = sys.argv[3 + num_modules]
+    code_path, test_path, output_path, extra_args = parse_args(sys.argv)
 
     # TODO: check slashes in windows and linux.
     if not os.path.exists(output_path):
@@ -23,14 +38,14 @@ if __name__ == "__main__":
     # TODO: how to not run coverage on __init__?
     os.system(
         "python -m pytest --cov-report annotate:%(cov_annotate)s --cov-report xml:%(Covxml)s %(cov_modules)s"
-        " %(test_path)s --junitxml=%(Testsxml)s" % {
+        " %(test_path)s --junitxml=%(Testsxml)s %(extra_args)s" % {
             "cov_modules": cov_modules,
             "test_path": test_path,
             "Covxml": output_path + "/coverage.xml",
             "Testsxml": output_path + "/tests.xml",
-            "cov_annotate": output_path + "/annotate"})
+            "cov_annotate": output_path + "/annotate",
+            "extra_args": extra_args})
     parser = Parser(db, output_path)
 
-    # python main.py code1 Tests_Examples\b_test.py Results
-    # python main.py 1 . Tests_Examples\ Results
+    # python main.py module=. tests=Tests_Examples\
 
