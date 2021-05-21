82c270e8342a9d67baea3c49c52d63d1332fd449
shaked.kialy <shaked.kialy@huji.mail.ac.il>
2021-05-21 12:37:45 +0300

    support exit code

--- main.py
+++ main.py
@@ -3,33 +3,28 @@ from Parser import *
 from DatabaseHandler import *
 from html_files.HTML import *
 
-NUM_ARGS = 5
-
 
 def parse_args(argv):
-    code_path, test_path, output_path, extra_args, delete_out, omit = "", "Tests", "Results", "", True, ""
-    for arg in argv:
+    code_path, test_path, output_path, extra_args, delete_out = "", "Tests", "Results", "", True
+    for arg in argv[1:]:
         if "module" in arg:
             modules = arg.split("=")[1]
             code_path = modules.split(",")
-        if "tests" in arg:
+        elif "tests" in arg:
             test_path = arg.split("=")[1]
-        if "out_dir" in arg:
+        elif "out_dir" in arg:
             output_path = arg.split("=")[1]
-        if "delete_out" in arg:
+        elif "delete_out" in arg:
             delete_out = arg.split("=")[1]
-        if "omit" in arg:
-            omit = arg.split("=")[1].split(",")
-        if len(sys.argv) > NUM_ARGS:
-            extra_args = sys.argv[NUM_ARGS:len(sys.argv)]
-        extra_args = " ".join(extra_args)
+        else:
+            extra_args += " " + arg
     if code_path == "":
         raise Exception("Missing modules to cover")
-    return code_path, test_path, output_path, omit, delete_out, extra_args
+    return code_path, test_path, output_path, delete_out, extra_args
 
 
 if __name__ == "__main__":
-    code_path, test_path, output_path, omit, delete_out, extra_args = parse_args(sys.argv)
+    code_path, test_path, output_path, delete_out, extra_args = parse_args(sys.argv)
 
     # TODO: check slashes in windows and linux.
     if not os.path.exists(output_path):
@@ -47,15 +42,7 @@ if __name__ == "__main__":
     if not delete_out and os.path.exists(output_path):
         output_path += str(db.get_last_run_id() + 1)
 
-    #TODO: not add file twice
-    #TODO: how to remove?
-    if omit:
-        with open('.coveragerc', 'a') as file:
-            for file_to_omit in omit:
-                file.write('\n       ' + file_to_omit)
-
-
-    os.system(
+    exit_code = os.system(
         "python -m pytest --cov-report annotate:%(cov_annotate)s --cov-report html:%(cov_html)s --cov-report xml:%(Covxml)s %(cov_modules)s"
         " %(test_path)s --junitxml=%(Testsxml)s --html=%(pytest_report)s %(extra_args)s" % {
             "cov_modules": cov_modules,
@@ -67,16 +54,18 @@ if __name__ == "__main__":
             "pytest_report": output_path + "/pytest_report.html",
             "extra_args": extra_args})
 
+    if exit_code != 0:
+        exit()
+
     parser = Parser(db, output_path)
 
     html.generating_html()
 
     if delete_out:
-        os.system("rm -r %(Covxml)s %(Testsxml)s %(cov_annotate)s .hypothesis .pytest_cache" % {
+        os.system("rm -r %(Covxml)s %(Testsxml)s %(cov_annotate)s .pytest_cache" % {
             "Covxml": output_path + "\\coverage.xml",
             "Testsxml": output_path + "\\tests.xml",
             "cov_annotate": output_path + "\\annotate"
-            # ,"cov_html": output_path + "\\html"
         })
 
     # python main.py module=. tests=Tests_Examples
